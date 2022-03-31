6f15d74f9fa27b75de68746205c07ea8c5a6b613
desiree-becerra <102554550+desiree-becerra@users.noreply.github.com>
2022-03-31 15:53:11 -0500

    Created a class exercise

---
+++ test1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:38:54 2022
+
+@author: becer
+"""
+
+print('Hello!')
+
+print('My name is Desiree')
+
+print('Some basic math')
+
+x=25
+
+y=10
+
+print(x+y)
